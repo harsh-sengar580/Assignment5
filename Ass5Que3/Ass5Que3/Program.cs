15f54d4cb1799d2715eb047269d670985996e242
harsh-sengar580 <142768094+harsh-sengar580@users.noreply.github.com>
2023-11-06 15:15:38 +0530

    Add files via upload

---
+++ Ass5Que3/Ass5Que3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass5Que3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "123Harshit"; 
+
+            try
+            {
+                int number = int.Parse(userInput);
+                Console.WriteLine("Parsed integer: " + number);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program completed.");
+            }
+        }
+    }
+    }
+
